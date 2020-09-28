9f44fbb86cf0fd9c1556b8148befb3163267d745
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-09-28 20:08:31 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Footer from './Page/Footer';
+import MenuMakanan from './Page/MenuMakanan';
+
+
+function App() {
+  return (
+    <div>
+        <h1>SEMENTARA SEPERTI INI, FILE HEADER DAN FOOTER ADA DI PC KANTOR</h1>
+        <MenuMakanan />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
